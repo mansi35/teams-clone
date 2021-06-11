2e6b03f2f738600bd9f20d266daf4ae594320bc2
Mansi Sharma <mansi.isha.mansi@gmail.com>
2021-06-11 11:40:32 +0530

    add initial backend setup

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://mansi35:V8NuYq5MCsEGEuz@teams-clone.iaugd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
+
+const db = mongoose.connection;
+db.once('open', () => console.log('Connected to Mongoose'));
+db.on('error', error => console.log(error));
